d24ef2ce7dc7fd19c8ad6e2ca8a6eeb995c41afd
foxwayne1 <fox.wayne1@gmail.com>
2023-05-24 13:34:35 +0100

    plus, minus ,and reset working

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Hello Wayne')
+const increaseBtn = document.querySelector('.counter__button--increase')
+const decreaseBtn = document.querySelector('.counter__button--decrease')
+const counterValue = document.querySelector('.counter__value')
+const counterResetBtn = document.querySelector('.counter__reset-button')
+
+let globalCounter = 0
+
+counterResetBtn.addEventListener('click', function () {
+    counterValue.textContent = +0
+})
+
+decreaseBtn.addEventListener('click', function () {
+    const currentValue = counterValue.textContent
+
+    const currentValueAsNumber = +currentValue
+
+    const newValue = currentValueAsNumber - 1
+
+    counterValue.textContent = newValue
+
+})
+
+increaseBtn.addEventListener('click', function () {
+    const currentValue = counterValue.textContent
+
+    const currentValueAsNumber = +currentValue
+
+    const newValue = currentValueAsNumber + 1
+
+    counterValue.textContent = newValue
+})
+
+
