b2bf1e4aa2d58097f756ef2c044816168e36bf93
foxwayne1 <fox.wayne1@gmail.com>
2023-05-24 14:25:16 +0100

    refactor to function for counter

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const decreaseBtn = document.querySelector('.counter__button--decrease')
 const counterValue = document.querySelector('.counter__value')
 const counterResetBtn = document.querySelector('.counter__reset-button')
 
-let globalCounter = 0
+function incrementCounter() {
+    const currentValue = counterValue.textContent
+    const currentValueAsNumber = +currentValue
+    const newValue = currentValueAsNumber + 1
+    counterValue.textContent = newValue
+}
 
 counterResetBtn.addEventListener('click', function () {
     counterValue.textContent = +0
@@ -14,20 +19,14 @@ decreaseBtn.addEventListener('click', function () {
 
     const currentValueAsNumber = +currentValue
 
-    const newValue = currentValueAsNumber - 1
+    const newValue = currentValueAsNumber - 1;
+    if(newValue < 0) return // dont go below 0
 
     counterValue.textContent = newValue
 
 })
 
-increaseBtn.addEventListener('click', function () {
-    const currentValue = counterValue.textContent
-
-    const currentValueAsNumber = +currentValue
-
-    const newValue = currentValueAsNumber + 1
-
-    counterValue.textContent = newValue
-})
+increaseBtn.addEventListener('click', incrementCounter)
 
 
+document.addEventListener('keydown', incrementCounter)
