3a64b1cd38336feb4d55764a02ebc470edd52d20
foxwayne1 <fox.wayne1@gmail.com>
2023-05-24 14:41:26 +0100

    reset and new html and css class done

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ const increaseBtn = document.querySelector('.counter__button--increase')
 const decreaseBtn = document.querySelector('.counter__button--decrease')
 const counterValue = document.querySelector('.counter__value')
 const counterResetBtn = document.querySelector('.counter__reset-button')
+const counterEl = document.querySelector('.counter')
+const counterTitle = document.querySelector('.counter__title')
 
 function incrementCounter() {
     const currentValue = counterValue.textContent
     const currentValueAsNumber = +currentValue
-    const newValue = currentValueAsNumber + 1
+    let newValue = currentValueAsNumber + 1;
+
+    if (newValue > 5) {
+        newValue = 5
+        counterEl.classList.add('counter--limit')
+        counterTitle.innerHTML = `Limit! Buy <strong>Pro</strong> for $5`
+
+        increaseBtn.disabled = true
+        decreaseBtn.disabled = true
+    }
+
+
     counterValue.textContent = newValue
 }
 
 counterResetBtn.addEventListener('click', function () {
     counterValue.textContent = +0
+    counterEl.classList.remove('counter--limit')
+    counterTitle.innerHTML = `Nice Counter`
+    increaseBtn.disabled = false
+    decreaseBtn.disabled = false
 })
 
 decreaseBtn.addEventListener('click', function () {
@@ -20,10 +37,9 @@ decreaseBtn.addEventListener('click', function () {
     const currentValueAsNumber = +currentValue
 
     const newValue = currentValueAsNumber - 1;
-    if(newValue < 0) return // dont go below 0
+    if (newValue < 0) return // dont go below 0
 
     counterValue.textContent = newValue
-
 })
 
 increaseBtn.addEventListener('click', incrementCounter)
